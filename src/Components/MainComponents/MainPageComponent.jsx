a44f2eb18a65ff12c9e71509639420dd84ac03ed
Sunmeet Singh <renu.kabeer@gmail.com>
2022-10-03 11:58:27 +0530

    Navbar fixed

--- src/Components/MainComponents/MainPageComponent.jsx
+++ src/Components/MainComponents/MainPageComponent.jsx
@@ -3,35 +3,33 @@ import React from 'react'
 export const MainPageComponent = () => {
     return (
         <div className='mainMainPageComponent'>
-            <div className="mainPage">
-                <div className="middle">
-                    <div className="tagLine">
-                        India's Coolest Travel Community
-                        <div className='typewriter'>
-                            <h6>Creating Memories</h6>
-                        </div>
-                    </div>
-                    <div className="searchBar">
-                        <input className='searchInput' type="text" />
-                        <img className='searchLogo' src="https://wanderon.in/svg/search.svg" alt="" />
+            <div className="middle">
+                <div className="tagLine">
+                    India's Coolest Travel Community
+                    <div className='typewriter'>
+                        <h6>Creating Memories</h6>
                     </div>
                 </div>
-                <div className="footer">
-                    <div>
-                        <img src="https://wanderon.in/svg/cover-travellers.svg" alt="" />
-                        <p>50,000+ <br />
-                            Happy Travellers</p>
-                    </div>
-                    <div>
-                        <img src="https://wanderon.in/svg/cover-travellers.svg" alt="" />
-                        <p>50,000+ <br />
-                            Happy Travellers</p>
-                    </div>
-                    <div>
-                        <img src="https://wanderon.in/svg/cover-travellers.svg" alt="" />
-                        <p>50,000+ <br />
-                            Happy Travellers</p>
-                    </div>
+                <div className="searchBar">
+                    <input className='searchInput' type="text" />
+                    <img className='searchLogo' src="https://wanderon.in/svg/search.svg" alt="" />
+                </div>
+            </div>
+            <div className="footer">
+                <div>
+                    <img src="https://wanderon.in/svg/cover-travellers.svg" alt="" />
+                    <p>50,000+ <br />
+                        Happy Travellers</p>
+                </div>
+                <div>
+                    <img src="https://wanderon.in/svg/cover-travellers.svg" alt="" />
+                    <p>50,000+ <br />
+                        Happy Travellers</p>
+                </div>
+                <div>
+                    <img src="https://wanderon.in/svg/cover-travellers.svg" alt="" />
+                    <p>50,000+ <br />
+                        Happy Travellers</p>
                 </div>
             </div>
         </div>
